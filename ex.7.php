053101a13163181d9dd36bd8dd7e95aebd60b7f5
sivenova <petia.sivenova@abv.bg>
2021-11-02 23:41:02 +0200

    homework

---
+++ ex.7.php
@@ -0,0 +1,37 @@
+<?php
+
+$number = 1;
+
+//Праим цикъл, който ще отпечатва броя редове
+for($i = 1; $i <= 5; $i++){
+    
+for($j = 1; $j <=$j; $j++){
+  //Отпечатваме числата като инкрементираме броя на итерациите с единица   
+    echo $number++ . "";
+}
+//отпечтваме нов ред
+echo '</br>';
+}
+echo '</br>';
+
+$numberTen = 1;
+
+for($i = 1; $i<=10; $i++){
+    for($j = 1; $j <=$i; $j++){
+        
+        echo $numberTen++ . "";
+    }
+    
+    echo'</br>';
+}
+echo '</br>';
+
+$numberTwentyOne = 1;
+for($i = 1; $i <= 21; $i++){
+    
+    for($j = 1; $j <= $i; $j++){
+        
+     echo $numberTwentyOne++ . "";
+    }
+    echo '</br>';
+}
